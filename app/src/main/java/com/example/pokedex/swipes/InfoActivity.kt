b68334b6bd0091e4fcf869142f7ba02df20684a3
L <ll6209@rit.edu>
2023-06-16 15:20:26 -0400

    all problems has been resolved relating to fetching data from previous activity, time to take a break

--- app/src/main/java/com/example/pokedex/swipes/InfoActivity.kt
+++ app/src/main/java/com/example/pokedex/swipes/InfoActivity.kt
@@ -1,37 +1,40 @@
 package com.example.pokedex.swipes
 
 import Pokemon
-import android.graphics.drawable.Drawable
-import android.icu.number.IntegerWidth
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.example.pokedex.R
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.FirebaseApp
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class InfoActivity : AppCompatActivity() {
-    private lateinit var viewPager : ViewPager2;
-    private lateinit var tabLayout: TabLayout;
-    private lateinit var pageAdapter : PageAdapter;
+    private lateinit var viewPager : ViewPager2
+    private lateinit var tabLayout: TabLayout
+    private lateinit var pageAdapter : PageAdapter
 
-    private lateinit var id : String;
-    private lateinit var sprite : String;
-    private lateinit var type1 : String;
-    private lateinit var type2 : String;
+    private lateinit var id : String
+    private lateinit var name : String
+    private lateinit var sprite : String
+    private lateinit var type1 : String
+    private lateinit var type2 : String
 
-    private lateinit var database : FirebaseDatabase
+    private val database = Firebase.database
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseApp.initializeApp(this)
         setContentView(R.layout.status_page)
         tabsContentSwitch()
         receiveDataFromPreviousActivity()
@@ -39,19 +42,23 @@ class InfoActivity : AppCompatActivity() {
 
 
 
-    private fun loadDataFromDB(index: Int, callback: (abilities: List<Any>?, base_exp : Int, effort : Map<String,Integer>, moves : Map<String,Map<String, Integer>>, stats : Map<String,Integer>)-> Unit) {
+    private fun loadDataFromDB(index: Int, callback: (abilities: Map<String,Boolean>, base_exp : Int, effort : Map<String,Long>, moves : Map<String,Map<String, Any>>, stats : Map<String,Long>)-> Unit) {
         val myRef = database.getReference("$index")
 
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val value =
-                    dataSnapshot.value as HashMap<Any, Any>? // Retrieve the value from the snapshot
-                val abilities = value?.get("abilities") as? List<Any>
-                val base_exp = value?.get("base_exp") as? Int
-                val effort = value?.get("effort") as? Map<String,Integer>
-                val moves = value?.get("moves") as Map<String,Map<String,Integer>>
-                val stats = value?.get("stats") as Map<String, Integer>
-                callback(abilities,base_exp!!, effort!!, moves,stats)
+                    dataSnapshot.value as HashMap<*, *>? // Retrieve the value from the snapshot
+                val abilities = value?.get("abilities") as? Map<String,Boolean>
+                val base_exp = value!!["base_exp"].toString().toInt()
+                val effort = value["effort"] as? Map<String, Long>
+                val moves = value["moves"] as Map<String, Map<String, Any>>
+                val stats = value["stats"] as Map<String, Long>
+
+                callback(
+                    abilities!!, base_exp!!,
+                    effort!! , moves, stats
+                )
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -62,9 +69,14 @@ class InfoActivity : AppCompatActivity() {
     private fun receiveDataFromPreviousActivity()
     {
         id = intent.getStringExtra("id")!!
+        name = intent.getStringExtra("name")!!
         sprite = intent.getStringExtra("sprite")!!
         type1 = intent.getStringExtra("type1")!!
-        type2 = intent.getStringExtra("type2")!!
+        type2 = intent.getStringExtra("type2") ?: ""
+        id = id.replace("§","").trim()
+
+        findViewById<TextView>(R.id.pokedexPokemonName).text = name;
+        findViewById<TextView>(R.id.pokedexID).text = id;
 
         loadDataFromDB(id.toInt()) { abilities, baseExp, effort, moves, stats ->
             // update ui based on data called back from fetch
@@ -73,18 +85,17 @@ class InfoActivity : AppCompatActivity() {
                 abilitiesList ->
                 for(ability in abilitiesList)
                 {
-                    if(ability != null)
-                    {
-                        //load into ui
-                    }
+                    val name = ability;
+                    //load into ui
                 }
             }
 
             effort?.let {
                 effortMap ->
-                for(keys in effortMap.keys)
+                for(key in effortMap.keys)
                 {
-                    val value = effortMap[keys]
+                    val name = key;
+                    val value = effortMap[key]?.toInt()
                 }
             }
 
@@ -92,6 +103,7 @@ class InfoActivity : AppCompatActivity() {
                 movesMap ->
                 for(key in movesMap.keys)
                 {
+                    val name = key;
                     val move = movesMap[key]
                     val accuracy = move?.get("accuracy")
                     val lvlReq = move?.get("lvl")
@@ -105,10 +117,8 @@ class InfoActivity : AppCompatActivity() {
                 statsMap ->
                 for(key in statsMap.keys)
                 {
-                    if(key != null)
-                    {
-                        val value = statsMap.get(key)
-                    }
+                    val name = key;
+                    val value = statsMap[key]
                 }
             }
 
