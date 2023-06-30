fc5e8ce5dfbb5be6d1345ca99a5a299c91d8398a
L <ll6209@rit.edu>
2023-06-30 01:23:39 -0400

    problem fixed, it was a rookie mistake, now just gotta upload images

--- app/src/main/java/com/example/pokedex/adapter_class/MoveAdapter.kt
+++ app/src/main/java/com/example/pokedex/adapter_class/MoveAdapter.kt
@@ -3,6 +3,7 @@ package com.example.pokedex.adapter_class
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.R
@@ -10,7 +11,7 @@ import com.example.pokedex.data_class.Move
 
 class MoveAdapter(private val moveList: Map<Int, Move>) : RecyclerView.Adapter<MoveAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_move, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_move, parent, false)
 
         return ViewHolder(view)
     }
@@ -18,9 +19,9 @@ class MoveAdapter(private val moveList: Map<Int, Move>) : RecyclerView.Adapter<M
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val move = moveList[position]
         holder.nameTextView.text = move!!.name
-        holder.typeTextView.text = move.type
+//        holder.typeTextView.text = move.type
         holder.levelTextView.text = move.lvl.toString()
-        holder.categoryTextView.text = move.category
+//        holder.categoryTextView.text = move.category
         holder.ppTextView.text = move.pp.toString()
         holder.powerTextView.text = move.power.toString()
         holder.accuracyTextView.text = move.accuracy.toString()
@@ -32,12 +33,12 @@ class MoveAdapter(private val moveList: Map<Int, Move>) : RecyclerView.Adapter<M
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val nameTextView: TextView = itemView.findViewById(R.id.moveNameCol)
-        val levelTextView : TextView = itemView.findViewById(R.id.levelCol)
-        val typeTextView: TextView = itemView.findViewById(R.id.typeCol)
-        val categoryTextView : TextView = itemView.findViewById(R.id.categoryCol)
-        val ppTextView : TextView = itemView.findViewById(R.id.ppCol)
-        val powerTextView : TextView = itemView.findViewById(R.id.powerCol)
-        val accuracyTextView : TextView = itemView.findViewById(R.id.accuracyCol)
+        val nameTextView: TextView = itemView.findViewById(R.id.moveNameFill)
+        val levelTextView : TextView = itemView.findViewById(R.id.levelFill)
+        val typeTextView: ImageView = itemView.findViewById(R.id.typeFill)
+        val categoryTextView : ImageView = itemView.findViewById(R.id.categoryFill)
+        val ppTextView : TextView = itemView.findViewById(R.id.ppFill)
+        val powerTextView : TextView = itemView.findViewById(R.id.powerFill)
+        val accuracyTextView : TextView = itemView.findViewById(R.id.accuracyFill)
     }
 }
